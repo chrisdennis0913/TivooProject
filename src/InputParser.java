3d468c0617a1d4c7b28562dca0e0daee31e9df29
Chris Dennis <chrisdennis0913@gmail.com>
2012-02-16 01:01:28 -0500

    Topher's stuff

--- src/InputParser.java
+++ src/InputParser.java
@@ -4,11 +4,11 @@ public class InputParser
     public static class ParserFactory{
         public static InputParser generate(String filename){
             if(filename.equals("DukeBasketBall.xml")) return new DukeBasketBallParser();
-            else if (filename.equals("GoogleCalendar")) return new GoogleCalendarParser();
+            else if (filename.equals("GoogleCalendar")) return new XMLtvParser();
             else return null;
         }
     }
     
 
-    
+
 }
